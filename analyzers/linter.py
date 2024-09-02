00f6d95faf2d7fd67ab63d68768c3f0fac5a5ea4
5hayanB <baig.sh@tutanota.com>
2024-09-02 21:18:14 +0500

    code refactor

--- analyzers/linter.py
+++ analyzers/linter.py
@@ -3,5 +3,5 @@ import subprocess
 
 def lint(file_path):
     result = subprocess.run(['verilator', '--lint-only', file_path], capture_output=True, text=True)
-    return result.returncode
+    return not result.returncode
 
