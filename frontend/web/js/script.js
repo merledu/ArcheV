2ed37fe5ae68ab9b6c1c4049720375353be02179
AdeiaSaif <adeia2903saif@gmail.com>
2024-09-05 14:26:58 +0500

    Modified js Function

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -69,7 +69,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', table);
+
+    function table(){
     const { jsPDF } = window.jspdf;
     function exportTableToJson() {
         const table = document.querySelector('table');
@@ -81,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
               acc[header] = cells[index].innerText;
               return acc;
         }, {});
-      });
+    });
       return data;
     }
 
@@ -99,8 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     document.getElementById('save-json').addEventListener('click', downloadJson);
-  });
-
+  }
 
   function scrollToTop() {
       window.scrollTo(0, 0);
