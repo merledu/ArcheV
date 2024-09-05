3b6b72527ada0bc47bcddc59008580ab0c8c4cb6
MahaR54 <maharehan.w.@gmail.com>
2024-09-05 12:56:48 +0500

    adding syntactical and functional verfication code

--- frontend/index.py
+++ frontend/index.py
@@ -57,5 +57,33 @@ def analyze(llm_path, context_length, gpu_layers):
                 llm_verilog_code=llm_response(SYSTEM_PROMPTS,user_propmpts)
 
                 with open(ARCHEV_TMP,'w') as tmp:
-                    tmp.write(llm_verilog_code)      
+                    tmp.write(llm_verilog_code)
+                          
+                syntactical_verification = lint(llm_verilog_code)
+            if syntactical_verification == "passed":
+                # Find json and prompt file name
+                prompt_name_without_extension = remove_extension(prompts_filename)
+                json_str = None
+                # shayan bhai ko check krwana hai kai json_str = json_files.get(prompt_name_without_extension, None)
+                for j, json_file in enumerate(json_files):
+                    json_name_without_extension = remove_extension(json_files[j])
+                    if prompt_name_without_extension == json_name_without_extension:
+                        json_str = json_file
+                        break
+                
+                # Perform functional verification if jason file is present
+                if json_str:
+                    functional_verification = funct.functional_verification(llm_verilog_code, json_str)
+                else:
+                    functional_verification = "failed"
+            else:
+                functional_verification = "failed"
+
+            # Store the results in the dictionary
+            results[prompt_name_without_extension] = {
+                "syntactical_verification": syntactical_verification,
+                "functional_verification": functional_verification
+            }
+
     return results
+                    
