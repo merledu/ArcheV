4f933db56ce02e3fe441c32be662d3f93e6ed391
ShehrozKashif <sharooz57@gmail.com>
2024-09-05 18:33:45 +0500

    added file path and file name

--- frontend/index.py
+++ frontend/index.py
@@ -7,57 +7,23 @@ from analyzers.funct import funct
 
 
 
-def read_file_content(file):
-    
-        return file.read().strip()
-
-
-def read_files_from_directory(directory_path, extension):
-    filenames = []
-    contents = []
-
-    # Check if the directory exists
-    if not os.path.exists(directory_path):
-        return filenames, contents
-
-    # List files in the directory
-    files_in_directory = os.listdir(directory_path)
-
-    for filename in files_in_directory:
-        if filename.endswith(extension):
-            file_path = os.path.join(directory_path, filename)
-            content = read_file_content(file_path)
-            filenames.append(filename)
-            contents.append(content)
-    return contents, filenames
-
-
-# Function to remove the file extension
-def remove_extension(filename):
-    return os.path.splitext(filename)[0]
-
-propmpts_path =  ARCHEV_PROMPTS 
-prompts, prompts_filename = read_files_from_directory(propmpts_path, ".txt")
-json_file_path = FUNCT_REF 
-json_files, json_names = read_files_from_directory(json_file_path, ".json")  
-
-
 
 def analyze(llm_path, context_length, gpu_layers):
     results = {} 
-    for root,sub_dir,files in os.walk(ARCHEV_PROMPTS):
-            for file in files:
+    for root , sub_dir, files in os.walk(ARCHEV_PROMPTS):
+        for file in files:
                 file_path = os.path.join(root, file)  
+                file_name = os.path.splitext(file)[0]
                 with open(file_path, "r") as p:
                     user_prompts = p.read() 
-                llm_verilog_code=llm_response(SYSTEM_PROMPTS,user_prompts)
+                llm_verilog_code=llm_response(SYSTEM_PROMPTS , user_prompts)
                 with open(ARCHEV_TMP,'w') as tmp:
                     tmp.write(llm_verilog_code)
                           
                 syntactical_verification = lint(ARCHEV_TMP)
-            if syntactical_verification == "passed":
+                if syntactical_verification == "passed":
                 # Find json and prompt file name
-                prompt_name_without_extension = remove_extension(prompts_filename)
+                    prompt_name_without_extension = remove_extension(prompts_filename)
                 json_str = None
                 # shayan bhai ko check krwana hai kai json_str = json_files.get(prompt_name_without_extension, None)
                 for j, json_file in enumerate(json_files):
@@ -71,11 +37,11 @@ def analyze(llm_path, context_length, gpu_layers):
                     functional_verification = funct.functional_verification(llm_verilog_code, json_str)
                 else:
                     functional_verification = "failed"
-            else:
+        else:
                 functional_verification = "failed"
 
             # Store the results in the dictionary
-            results[prompt_name_without_extension] = {
+                results[prompt_name_without_extension] = {
                 "syntactical_verification": syntactical_verification,
                 "functional_verification": functional_verification
             }
