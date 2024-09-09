9f1cc1a746db37cf40c65b8cbe3216806bd5a7e2
ShehrozKashif <sharooz57@gmail.com>
2024-09-09 12:58:48 +0500

    refactored analyze function

--- frontend/index.py
+++ frontend/index.py
@@ -8,10 +8,10 @@ from analyzers.funct import functional_verification
 
 
 def analyze(llm_path, context_length, gpu_layers):
-    load_llm(llm_path,context_length,gpu_layers)
+    load_llm(llm_path, context_length, gpu_layers)
     results = {} 
     
-    for root , sub_dir, files in os.walk(ARCHEV_PROMPTS):
+    for root, sub_dir, files in os.walk(ARCHEV_PROMPTS):
         for id_count, file in enumerate(files):
             file_path = os.path.join(root, file)  
             file_name = os.path.splitext(file)[0]
@@ -20,17 +20,11 @@ def analyze(llm_path, context_length, gpu_layers):
             llm_verilog_code = llm_response(SYSTEM_PROMPTS, user_prompt)
             with open(TMP_VERILOG,'w', encoding="utf-8") as tmp:
                 tmp.write(llm_verilog_code)
-            verify_syntactically =  lint(llm_verilog_code)            
-            if verify_syntactically:
-                y = json.loads(file)
-                verify_functionally = functional_verification(file_name)
-            else:
-                verify_functionally = "failed"
-            # Store the results in the dictionary
+            verify_syntactically = lint(llm_verilog_code)  
+            verify_functionally = verify_syntactically(file_name) if verify_syntactically else "False"       
         results[id_count] = {
             "Prompt" : file_name,
             "syntactical_verification": verify_syntactically,
             "functional_verification": verify_functionally
         }
     return results
-
