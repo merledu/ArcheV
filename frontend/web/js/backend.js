69a4dec0da2f757c7cf06c53286b392a079cc0a8
ShehrozKashif <sharooz57@gmail.com>
2024-09-18 23:20:29 +0500

    added display table

--- frontend/web/js/backend.js
+++ frontend/web/js/backend.js
@@ -18,35 +18,55 @@ function analyzes() {
     updateProgressBar(100); 
   }
   }
-  x
   };
 
+    function display_table(x){
+    let table_body = document.createElement("#dictable tbody")
 
-function updateTable(data) {
-    const tableBody = document.querySelector('table tbody');
-    tableBody.innerHTML = '';
-    data.forEach((item, index) => {
-        const row = document.createElement('tr');
-        const idCell = document.createElement('td');
-        idCell.textContent = index + 1;
-        row.appendChild(idCell);
-        const moduleNameCell = document.createElement('td');
-        moduleNameCell.textContent = item[2]; 
-        row.appendChild(moduleNameCell);
-        const syntacticalCell = document.createElement('td');
-        const syntacticalStatus = item[0]; 
-        const syntacticalText = document.createElement('p');
-        syntacticalText.className = syntacticalStatus === 'True' ? 'status delivered' : 'status cancelled';
-        syntacticalText.textContent = syntacticalStatus === 'True' ? 'True' : 'False';
-        syntacticalCell.appendChild(syntacticalText);
-        row.appendChild(syntacticalCell);
-        const functionalCell = document.createElement('td');
-        const functionalStatus = item[1]; 
-        const functionalText = document.createElement('p');
-        functionalText.className = functionalStatus === 'True' ? 'status delivered' : 'status cancelled';
-        functionalText.textContent = functionalStatus === 'True' ? 'True' : 'False';
-        functionalCell.appendChild(functionalText);
-        row.appendChild(functionalCell);
-        tableBody.appendChild(row);
-    });
-}
+    for(let key in x){
+
+      let row = document.createElement("tr");
+      let value = document.createElement("td");
+      value.textContent = dictionary[key];
+      row.appendChild(value);
+
+      table_body.appendChild(row);
+      
+
+
+
+
+
+
+    }
+    }    
+
+
+// function updateTable(data) {
+//     const tableBody = document.querySelector('table tbody');
+//     tableBody.innerHTML = '';
+//     data.forEach((item, index) => {
+//         const row = document.createElement('tr');
+//         const idCell = document.createElement('td');
+//         idCell.textContent = index + 1;
+//         row.appendChild(idCell);
+//         const moduleNameCell = document.createElement('td');
+//         moduleNameCell.textContent = item[2]; 
+//         row.appendChild(moduleNameCell);
+//         const syntacticalCell = document.createElement('td');
+//         const syntacticalStatus = item[0]; 
+//         const syntacticalText = document.createElement('p');
+//         syntacticalText.className = syntacticalStatus === 'True' ? 'status delivered' : 'status cancelled';
+//         syntacticalText.textContent = syntacticalStatus === 'True' ? 'True' : 'False';
+//         syntacticalCell.appendChild(syntacticalText);
+//         row.appendChild(syntacticalCell);
+//         const functionalCell = document.createElement('td');
+//         const functionalStatus = item[1]; 
+//         const functionalText = document.createElement('p');
+//         functionalText.className = functionalStatus === 'True' ? 'status delivered' : 'status cancelled';
+//         functionalText.textContent = functionalStatus === 'True' ? 'True' : 'False';
+//         functionalCell.appendChild(functionalText);
+//         row.appendChild(functionalCell);
+//         tableBody.appendChild(row);
+//     });
+// }
