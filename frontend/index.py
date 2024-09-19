749a68f8e2d000f6d63c79b24f5d87341cbd561f
MahaR54 <maharehan.w.@gmail.com>
2024-09-19 22:56:39 +0500

    new changes added in index.py

--- frontend/index.py
+++ frontend/index.py
@@ -1,5 +1,4 @@
 import os
-import json
 
 from globals import ARCHEV_PROMPTS, SYSTEM_PROMPTS, TMP_VERILOG
 from llm_interface.llm_interface import load_llm, llm_response
@@ -8,37 +7,33 @@ from analyzers.funct import functional_verification
 
 
 def analyze(llm_path, context_length, gpu_layers):
-    pass
     load_llm(llm_path, context_length, gpu_layers)
-    results = {} 
-    progress = 0  
-    
-    walk_results = []
+    results = {}
+    progress = 0
+
     for root, sub_dir, files in os.walk(ARCHEV_PROMPTS):
-        walk_results.append((root, sub_dir, files))  
-    
-    # for root, sub_dir, files in os.walk(ARCHEV_PROMPTS):
-    for id_count, file in enumerate(walk_results[2]):
-            file_path = os.path.join(walk_results[0], file)  
+        total_files = len(files)
+        for id_count, file in enumerate(files):
+            file_path = os.path.join(root, file)
             file_name = os.path.splitext(file)[0]
+
             with open(file_path, "r", encoding="utf-8") as p:
-                user_prompt = p.read() 
+                user_prompt = p.read()
             llm_verilog_code = llm_response(SYSTEM_PROMPTS, user_prompt)
-            with open(TMP_VERILOG,'w', encoding="utf-8") as tmp:
+            with open(TMP_VERILOG, 'w', encoding="utf-8") as tmp:
                 tmp.write(llm_verilog_code)
             windows['main'].evaluate_js(
-            f'''
-            window.update_progress_bar({progress});
-            window.update_progress_label("Executing test {id_count + 1} of {len(walk_results[2])}: {file_name[id_count][1].replace('_', ' ')}");
-            '''
+                f'''
+                window.update_progress_bar({progress});
+                window.update_progress_label("Executing test {id_count + 1} of {total_files}: {file_name.replace('_', ' ')}");
+                '''
             )
-            verify_syntactically = lint(llm_verilog_code)  
-            verify_functionally = functional_verification(file_name) if verify_syntactically else False       
+            verify_syntactically = lint(llm_verilog_code)
+            verify_functionally = functional_verification(file_name) if verify_syntactically else False
             results[id_count] = {
-                "Prompt" : file_name,
+                "Prompt": file_name,
                 "syntactical_verification": verify_syntactically,
                 "functional_verification": verify_functionally
             }
-            progress= 99 / len(walk_results[2])
-            # progress += 1
+            progress = 99 / len(files)
     return results
